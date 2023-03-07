254ef4d9ed1a0e66e226575cba429947427e6290
andresspin <andresspin80@gmail.com>
2023-03-06 19:31:24 -0500

    Corregido total

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { legacy_createStore as createStore } from "redux";
-import messageReducer from "reducers/messageReducer";
+import messageReducer from "./reducers/messageReducer";
 
-function configureStore(state = { messsage: "Default message" }) {
+function configureStore(state = { message: "Default message" }) {
   return createStore(messageReducer, state);
 }
 
-export default configureStore;
+export default configureStore();
